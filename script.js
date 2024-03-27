dd1fea116533f30068e9ef938e891b648ec7b5b3
m4c130d <m4c130d@protonmail.com>
2024-03-28 00:07:33 +0100

    Finished product

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const board = document.querySelector(".board");
 const body = document.querySelector("body");
 const selectBtn = document.querySelector("#select");
-const redBtn = document.querySelector("#red");
-const blackBtn = document.querySelector("#black");
-const rainbowBtn = document.querySelector("#rainbow");
 const resetBtn = document.querySelector("#reset");
+const box = document.querySelectorAll(".box")
+const colorBtn = document.querySelectorAll(".color-btn");
 
+
+function randomize() {
+    Math.floor(Math.random() * 256)
+}
 
 
 function createRow(size) {
     for (i=0; i<size; i++) {
-        row = document.createElement("div");
+        let row = document.createElement("div");
         row.classList.add("row");
         board.appendChild(row);
     }
@@ -18,11 +21,11 @@ function createRow(size) {
 }
 
 function createBox(size) {
-    rows = document.querySelectorAll(".row");
+    let rows = document.querySelectorAll(".row");
     console.log(rows);
     rows.forEach(row => {
         for (i=0; i<size; i++) {
-            box = document.createElement("div");
+            let box = document.createElement("div");
             box.classList.add("box");
             row.appendChild(box);
         }
@@ -30,7 +33,7 @@ function createBox(size) {
 }
 
 function changeSize() {
-    size = prompt("choose a new size between 0 and 100");
+    let size = prompt("choose a new size between 0 and 100");
     deleteBoard();
     createRow(size);
 }
@@ -46,6 +49,31 @@ function resetBoard() {
     createRow(32);
 }
 
+function setColor(color) {
+    board.addEventListener('mouseover', (e) => {
+        if (color === 'rainbow') {
+            e.target.style.backgroundColor = `rgb(${randomize()},${randomize()},${randomize()})`;
+        } else {
+            e.target.style.backgroundColor = `${color}`;
+        }
+});
+}
+
+function randomize() {
+    return Math.floor(Math.random() * 256);
+}
+
 createRow(32);
 selectBtn.addEventListener('click', changeSize);
 resetBtn.addEventListener('click', resetBoard);
+
+colorBtn.forEach(button => {
+    button.addEventListener('click', (e) => {
+        let target = e.target        
+        setColor(target.id);
+    })}
+);
+
+
+
+
